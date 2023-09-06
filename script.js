75de03c97167261f593a582bb781f54a53cbed50
Ashley Rogers <130684731+arogersrenee@users.noreply.github.com>
2023-09-06 10:44:05 -0500

    add js and declare variables

---
+++ script.js
@@ -0,0 +1,17 @@
+const form = document.querySelector("form");
+const firstname = document.querySelector("#firstname");
+const firstnameIcon = document.querySelector("#firstname-icon");
+const firstnameMsg = document.querySelector("#firstname-msg");
+
+const lastname = document.querySelector("#lastname");
+const lastnameIcon = document.querySelector("#lastname-icon");
+const lastnameMsg = document.querySelector("#lastname-msg");
+
+const email = document.querySelector("#email");
+const emailIcon = document.querySelector("#email-icon");
+const emailMsg = document.querySelector("#email-msg");
+
+const password = document.querySelector("#password");
+const passwordIcon = document.querySelector("#password-icon");
+const passwordMsg = document.querySelector("#password-msg");
+
